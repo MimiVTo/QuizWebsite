715b1281fd7c53abb8fe4d0496a43a2a4464ef4d
Mimi To <MimivTo2007@gmai.com>
2024-11-12 23:12:42 -0800

    Worked on

--- script.js
+++ script.js
@@ -17,43 +17,111 @@ var questions={
         answer: "Sticks"
     },
     "questionFour":{
-        question: "",
-        options:[],
-        answer:
+        question: "What's Mimi's Favorite Food?",
+        options:["Pizza", "Chow Fun", "Sushi", "Shumai"],
+        answer: "Chow Fun"
     },
     "questionFive":{
-        question: "",
-        options:[],
-        answer:
+        question: "What's Mimi's favorite Manhwa?",
+        options:["Lookism", "Don't Pick Up The Trash You Threw Away", "Weak Hero", "Lumine"],
+        answer: "Weak Hero"
     },
     "questionSix":{
-        question: "",
-        options:[],
-        answer:
+        question: "Who's Mimi's Favorite Character?",
+        options:["Wolf Keum", "Joseph Desaulnier", "Bayonetta", "Yor Forger"],
+        answer: "Wolf Keum"
     },
     "questionSeven":{
         question: "",
         options:[],
-        answer:
+        answer:""
     },
     "questionEight":{
         question: "",
         options:[],
-        answer:
+        answer:""
     },
     "questionNine":{
         question: "",
         options:[],
-        answer:
-    }
+        answer:""
+    },
+    "questionTen":{
+        question: "",
+        options:[],
+        answer:""
+    },
+    "questionEleven":{
+        question: "",
+        options:[],
+        answer:""
+    },
+    "questionTwelve":{
+        question: "",
+        options:[],
+        answer:""
+    },
+    "questionThirteen":{
+        question: "",
+        options:[],
+        answer:""
+    },
+    "questionFourteen":{
+        question: "",
+        options:[],
+        answer:""
+    },
+    "questionFifteen":{
+        question: "",
+        options:[],
+        answer:""
+    },
+    "questionSixteen":{
+        question: "",
+        options:[],
+        answer:""
+    },
+    "questionSeventeen":{
+        question: "",
+        options:[],
+        answer:""
+    },
+    "questionEighteen":{
+        question: "",
+        options:[],
+        answer:""
+    },
+    "questionNineteen":{
+        question: "",
+        options:[],
+        answer:""
+    },
+    "questionTwenty":{
+        question: "",
+        options:[],
+        answer:""
+    },
 }
 
 // CONTAINERS -------------------------------------
 var questionContainer = document.getElementById("titleContainer");
 var responseContainer = document.getElementById("container");
 
+// OPTIONS FOR ANSWERING THE QUESTION -------------
+var optionA = document.getElementById("optionA");
+var optionB = document.getElementById("optionB");
+var optionC = document.getElementById("optionC");
+var optionD = document.getElementById("optionD");
+
 // GLOBAL BUTTONS ---------------------------------
 var next = document.getElementById("nextBtn");
+
+// BUTTONS PRESSED ------------
+
+var buttonA = document.getElementById("buttonA");
+var buttonB = document.getElementById("buttonB");
+var buttonC = document.getElementById("buttonC");
+var buttonD = document.getElementById("buttonD");
 
 // NUMBER TRACKING --------------------------------
 var question = 0;
@@ -61,8 +129,13 @@ var correctAnswers = 0;
 
 // FUNCTIONS --------------------------------------
 
-function quizBegin(){
+function loadedQuestion(){
+    //First question asked
+
+}
+
+function nextQuestion(){
     //Loads up the question and 
 }
 
-next.addEventListener("click", quizBegin);
+next.addEventListener("click", nextQuestion);
